d00d13a8f8165789ee617b4e590876c47b6b800f
zinualhambra <48044861+zinualhambra@users.noreply.github.com>
2021-04-01 19:38:53 +0800

    Add files via upload

---
+++ includes/menubar.php
@@ -0,0 +1,32 @@
+<aside class="main-sidebar">
+  <!-- sidebar: style can be found in sidebar.less -->
+  <section class="sidebar">
+    <!-- Sidebar user panel -->
+    <!-- <div class="user-panel">
+      <div class="pull-left image">
+        <img src="../images/icon.png" class="img-circle" alt="User Image">
+      </div>
+      <div class="pull-left info">
+        <p style="text-transform: uppercase;">< ?php echo $user['firstname'].' '.$user['lastname']; ?></p>
+        <p style="font-size:12px;">< ?php echo $user['email']; ?></p>
+        <a><i class="fa fa-circle text-success"></i> Online</a>
+      </div>
+    </div> -->
+    <!-- sidebar menu: : style can be found in sidebar.less -->
+    <ul class="sidebar-menu" data-widget="tree">
+      <li class="header">MENU</li> 
+      <li class=""><a href="home.php"><i class="fa fa-dashboard"></i> <span>Dashboard</span></a></li>
+      <li class=""><a href="castvote.php"><i class="fa fa-cube"></i> <span>Vote</span></a></li>
+      <li class=""><a href="result.php"><i class="fa fa-cube"></i> <span>Result</span></a></li>
+<!--  <li class="header">MANAGE</li>
+      <li class=""><a href="voters.php"><i class="fa fa-users"></i> <span>Voters</span></a></li>
+      <li class=""><a href="positions.php"><i class="fa fa-tasks"></i> <span>Positions</span></a></li>
+      <li class=""><a href="candidates.php"><i class="fa fa-black-tie"></i> <span>Candidates</span></a></li>
+      <li class="header">SETTINGS</li>
+      <li class=""><a href="ballot.php"><i class="fa fa-file-text"></i> <span>Ballot Position</span></a></li>
+      <li class=""><a href="#config" data-toggle="modal"><i class="fa fa-cog"></i> <span>Election Title</span></a></li> -->
+    </ul>
+  </section>
+  <!-- /.sidebar -->
+</aside>
+<?php include 'config_modal.php'; ?>
