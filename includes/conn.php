916d70bd0544abdd9f92aa909a42fc5c777db19b
zinualhambra <48044861+zinualhambra@users.noreply.github.com>
2021-04-01 22:56:38 +0800

    Add files via upload

--- includes/conn.php
+++ includes/conn.php
@@ -1,5 +1,5 @@
 <?php
-	$conn = new mysqli('localhost', 'root', 'xhqxHVzH', 'vote4me');
+	$conn = new mysqli('localhost', 'root', '', 'vote4me');
 
 	if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,8 @@
   	<meta charset="utf-8">
   	<meta http-equiv="X-UA-Compatible" content="IE=edge">
   	<title>VOTE4ME</title>
+    <link rel="icon" href="images/icon.png" type="image/icon type">
+
   	<!-- Tell the browser to be responsive to screen width -->
   	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   	<!-- Bootstrap 3.3.7 -->
@@ -14,13 +16,11 @@
     <link rel="stylesheet" href="bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css">
   	<!-- Font Awesome -->
   	<link rel="stylesheet" href="bower_components/font-awesome/css/font-awesome.min.css">
-  
   	<!-- Theme style -->
-  	<link rel="stylesheet" href="dist/css/AdminLTE3.css">
-  
+  	<link rel="stylesheet" href="dist/css/AdminLTE.min.css">
   	<!-- AdminLTE Skins. Choose a skin from the css/skins
        folder instead of downloading all of them to reduce the load. -->
-  	<link rel="stylesheet" href="dist/css/skins/_all-skins3.css">
+  	<link rel="stylesheet" href="dist/css/skins/_all-skins.min.css">
 
   	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
   	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
