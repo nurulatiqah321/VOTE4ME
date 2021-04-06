2096b5fa9bd0b73b9765f694bbd7ea6da05eb3c3
Nurul Atiqah <48888801+nurulatiqah321@users.noreply.github.com>
2021-04-06 22:52:35 +0800

    Update index.php

--- index.php
+++ index.php
@@ -6,34 +6,28 @@
 <title>VOTE4ME</title>
     <link rel="icon" href="images/icon.png" type="image/icon type">
 </head>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=DotGothic16&family=Nanum+Gothic&display=swap" rel="stylesheet">
         <!-- CSS File -->
         <link rel="stylesheet" href="mainpage.css">
-
         <style>
         body{
         background-image: url("images/homepagebg.png");
          }
         </style>
-
     </head>
     <body>
  
         <div class="flex-center position-ref full-height">
-
                 <div class="content">
                     <div class="text text-wrap">
                     <p>Your vote is your voice.</p>
                     <p>Raise, Get up and VOTE!!</p>
                     </div>
                 </div>
-
                 <div class="logo">
                     <img src="images/logo(blue).png" alt="logo">
                 </div>
-
                 <div class="top-right links">
                         <a href="studentlogin.php">STUDENT</a>
                         <a href="admin/index.php">ADMIN</a>
