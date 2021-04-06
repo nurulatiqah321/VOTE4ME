6d6e623adb237da8052c0f2bedb856aa8a1507ed
Nurul Atiqah <48888801+nurulatiqah321@users.noreply.github.com>
2021-04-06 22:51:49 +0800

    Update home.php

--- home.php
+++ home.php
@@ -1,8 +1,7 @@
 <?php include 'includes/session.php'; ?>
-<?php include 'includes/slugify.php'; ?>
 <?php include 'includes/header.php'; ?>
 <body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
+<div>
 
   <?php include 'includes/navbar.php'; ?> 
   <?php include 'includes/menubar.php'; ?>
@@ -20,7 +19,6 @@
       </ol>
     </section>
 
-    <!-- Main content -->
     <!-- Main content -->
 	      <section class="content">
 	      	<?php
@@ -75,84 +73,11 @@
       <!-- right col -->
     </div>
   	<?php include 'includes/footer.php'; ?>
-
+	
 </div>
 <!-- ./wrapper -->
 
 <?php include 'includes/scripts.php'; ?>
-<?php
-  $sql = "SELECT * FROM positions ORDER BY priority ASC";
-  $query = $conn->query($sql);
-  while($row = $query->fetch_assoc()){
-    $sql = "SELECT * FROM candidates WHERE position_id = '".$row['id']."'";
-    $cquery = $conn->query($sql);
-    $carray = array();
-    $varray = array();
-    while($crow = $cquery->fetch_assoc()){
-      array_push($carray, $crow['cname']);
-      $sql = "SELECT * FROM votes WHERE candidate_id = '".$crow['id']."'";
-      $vquery = $conn->query($sql);
-      array_push($varray, $vquery->num_rows);
-    }
-    $carray = json_encode($carray);
-    $varray = json_encode($varray);
-    ?>
-    <script>
-    $(function(){
-      var rowid = '<?php echo $row['id']; ?>';
-      var description = '<?php echo slugify($row['description']); ?>';
-      var barChartCanvas = $('#'+description).get(0).getContext('2d')
-      var barChart = new Chart(barChartCanvas)
-      var barChartData = {
-        labels  : <?php echo $carray; ?>,
-        datasets: [
-          {
-            label               : 'Votes',
-            fillColor           : 'rgba(60,141,188,0.9)',
-            strokeColor         : 'rgba(60,141,188,0.8)',
-            pointColor          : '#3b8bba',
-            pointStrokeColor    : 'rgba(60,141,188,1)',
-            pointHighlightFill  : '#fff',
-            pointHighlightStroke: 'rgba(60,141,188,1)',
-            data                : <?php echo $varray; ?>
-          }
-        ]
-      }
-      var barChartOptions                  = {
-        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-        scaleBeginAtZero        : true,
-        //Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines      : true,
-        //String - Colour of the grid lines
-        scaleGridLineColor      : 'rgba(0,0,0,.05)',
-        //Number - Width of the grid lines
-        scaleGridLineWidth      : 1,
-        //Boolean - Whether to show horizontal lines (except X axis)
-        scaleShowHorizontalLines: true,
-        //Boolean - Whether to show vertical lines (except Y axis)
-        scaleShowVerticalLines  : true,
-        //Boolean - If there is a stroke on each bar
-        barShowStroke           : true,
-        //Number - Pixel width of the bar stroke
-        barStrokeWidth          : 2,
-        //Number - Spacing between each of the X value sets
-        barValueSpacing         : 5,
-        //Number - Spacing between data sets within X values
-        barDatasetSpacing       : 1,
-        //String - A legend template
-        legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
-        //Boolean - whether to make the chart responsive
-        responsive              : true,
-        maintainAspectRatio     : true
-      }
 
-      barChartOptions.datasetFill = false
-      var myChart = barChart.HorizontalBar(barChartData, barChartOptions)
-      //document.getElementById('legend_'+rowid).innerHTML = myChart.generateLegend();
-    });
-    </script>
-    <?php
-  }
-?>
 </body>
 </html>
