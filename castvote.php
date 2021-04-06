7f49444cd3362282481997ab595b22a1925210cc
Nurul Atiqah <48888801+nurulatiqah321@users.noreply.github.com>
2021-04-06 22:50:00 +0800

    Update castvote.php

--- castvote.php
+++ castvote.php
@@ -1,26 +1,27 @@
 <?php include 'includes/session.php'; ?>
 <?php include 'includes/header.php'; ?>
 <body class="hold-transition skin-blue sidebar-mini">
-<div class="wrapper">
+<div>
 
   <?php include 'includes/navbar.php'; ?> 
   <?php include 'includes/menubar.php'; ?>
+  
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>
-        Dashboard
-      </h1>
-      <ol class="breadcrumb">
-        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-        <li class="active">Vote</li>
-      </ol>
-    </section>
+    <div class="content-wrapper">
+		<!-- Content Header (Page header) -->
+		<section class="content-header">
+			<h1>
+				Dashboard
+			</h1>
+			<ol class="breadcrumb">
+				<li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+				<li class="active">Vote</li>
+			</ol>
+		</section>
 
-    <!-- Main content -->
-    <!-- Main content -->
+	    <div class="container">
+
+	      <!-- Main content -->
 	      <section class="content">
 	      	<?php
 	      		$parse = parse_ini_file('admin/config.ini', FALSE, INI_SCANNER_RAW);
@@ -66,8 +67,8 @@
 			        	<span class="message"></span>
 			        </div>
 
-					<?php
-				    	$sql = "SELECT * FROM votes WHERE matricno = '".$voter['matricno']."'";
+				    <?php
+				    	$sql = "SELECT * FROM votes WHERE matricno = '".$voter['id']."'";
 				    	$vquery = $conn->query($sql);
 				    	if($vquery->num_rows > 0){
 				    		?>
@@ -113,10 +114,9 @@
 											$image = (!empty($crow['photo'])) ? 'images/'.$crow['photo'] : 'images/profile.jpg';
 											$candidate .= '
 												<li>
-													'.$input.'<button type="button" class="btn btn-primary btn-sm btn-flat clist platform" data-platform="'.$crow['platform'].'" data-fullname="'.$crow['cname'].'"><i class="fa fa-search"></i> Platform</button><img src="'.$image.'" height="100px" width="100px" class="clist"><span class="cname clist">'.$crow['cname'].'</span>
+												'.$input.'<button type="button" class="btn btn-primary btn-sm btn-flat clist platform" data-platform="'.$crow['platform'].'" data-fullname="'.$crow['cname'].'"><i class="fa fa-user"></i> Manisfesto</button><img src="'.$image.'" height="100px" width="100px" class="clist"><span class="cname clist">'.$crow['cname'].'</span>
 												</li>
 											';
-											// <li>'.$input.'<button type="button" class="btn btn-primary btn-sm btn-flat clist platform" data-platform="'.$crow['platform'].'" data-fullname="'.$crow['firstname'].' '.$crow['lastname'].'"><i class="fa fa-search"></i> Platform</button><img src="'.$image.'" height="100px" width="100px" class="clist"><span class="cname clist">'.$crow['firstname'].' '.$crow['lastname'].'</span></li>
 										}
 
 										$instruct = ($row['max_vote'] > 1) ? 'You may select up to '.$row['max_vote'].' candidates' : 'Select only one candidate';
@@ -129,12 +129,8 @@
 															<h3 class="box-title"><b>'.$row['description'].'</b></h3>
 														</div>
 														<div class="box-body">
-														<p>'.$instruct.'
-														<span class="pull-right">
-															<button type="button" class="btn btn-success btn-sm btn-flat reset" data-desc="'.slugify($row['description']).'"><i class="fa fa-refresh"></i> Reset</button>
-														</span>
-													</p>
-													<div id="candidate_list">
+															<p>'.$instruct.'</p>
+															<div id="candidate_list">
 																<ul>
 																	'.$candidate.'
 																</ul>
@@ -156,89 +152,79 @@
 					        	</div>
 				        	</form>
 				        	<!-- End Voting Ballot -->
-	        </div>
-	      </section>
-      <!-- right col -->
-    </div>
-  	<?php include 'includes/footer.php'; ?>
+				    		<?php
+				    	}
 
+				    ?>
+
+	        	</div>
+	        </div>
+			
+	      </section>
+	     
+	    </div>
+		<!-- container -->
+	</div>
+	<!-- content-wrapper -->
+  
+  	<?php include 'includes/footer.php'; ?>
+	<?php include 'includes/ballot_modal.php'; ?>
+	
+  	
 </div>
-<!-- ./wrapper -->
+<!-- wrapper -->
 
 <?php include 'includes/scripts.php'; ?>
-<?php
-  $sql = "SELECT * FROM positions ORDER BY priority ASC";
-  $query = $conn->query($sql);
-  while($row = $query->fetch_assoc()){
-    $sql = "SELECT * FROM candidates WHERE position_id = '".$row['id']."'";
-    $cquery = $conn->query($sql);
-    $carray = array();
-    $varray = array();
-    while($crow = $cquery->fetch_assoc()){
-      array_push($carray, $crow['cname']);
-      $sql = "SELECT * FROM votes WHERE candidate_id = '".$crow['id']."'";
-      $vquery = $conn->query($sql);
-      array_push($varray, $vquery->num_rows);
-    }
-    $carray = json_encode($carray);
-    $varray = json_encode($varray);
-    ?>
-    <script>
-    $(function(){
-      var rowid = '<?php echo $row['id']; ?>';
-      var description = '<?php echo slugify($row['description']); ?>';
-      var barChartCanvas = $('#'+description).get(0).getContext('2d')
-      var barChart = new Chart(barChartCanvas)
-      var barChartData = {
-        labels  : <?php echo $carray; ?>,
-        datasets: [
-          {
-            label               : 'Votes',
-            fillColor           : 'rgba(60,141,188,0.9)',
-            strokeColor         : 'rgba(60,141,188,0.8)',
-            pointColor          : '#3b8bba',
-            pointStrokeColor    : 'rgba(60,141,188,1)',
-            pointHighlightFill  : '#fff',
-            pointHighlightStroke: 'rgba(60,141,188,1)',
-            data                : <?php echo $varray; ?>
-          }
-        ]
-      }
-      var barChartOptions                  = {
-        //Boolean - Whether the scale should start at zero, or an order of magnitude down from the lowest value
-        scaleBeginAtZero        : true,
-        //Boolean - Whether grid lines are shown across the chart
-        scaleShowGridLines      : true,
-        //String - Colour of the grid lines
-        scaleGridLineColor      : 'rgba(0,0,0,.05)',
-        //Number - Width of the grid lines
-        scaleGridLineWidth      : 1,
-        //Boolean - Whether to show horizontal lines (except X axis)
-        scaleShowHorizontalLines: true,
-        //Boolean - Whether to show vertical lines (except Y axis)
-        scaleShowVerticalLines  : true,
-        //Boolean - If there is a stroke on each bar
-        barShowStroke           : true,
-        //Number - Pixel width of the bar stroke
-        barStrokeWidth          : 2,
-        //Number - Spacing between each of the X value sets
-        barValueSpacing         : 5,
-        //Number - Spacing between data sets within X values
-        barDatasetSpacing       : 1,
-        //String - A legend template
-        legendTemplate          : '<ul class="<%=name.toLowerCase()%>-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
-        //Boolean - whether to make the chart responsive
-        responsive              : true,
-        maintainAspectRatio     : true
-      }
+<script>
+$(function(){
+	$('.content').iCheck({
+		checkboxClass: 'icheckbox_flat-green',
+		radioClass: 'iradio_flat-green'
+	});
 
-      barChartOptions.datasetFill = false
-      var myChart = barChart.HorizontalBar(barChartData, barChartOptions)
-      //document.getElementById('legend_'+rowid).innerHTML = myChart.generateLegend();
-    });
-    </script>
-    <?php
-  }}
-?>
+	$(document).on('click', '.platform', function(e){
+		e.preventDefault();
+		$('#platform').modal('show');
+		var platform = $(this).data('platform');
+		var fullname = $(this).data('fullname');
+		$('.candidate').html(fullname);
+		$('#plat_view').html(platform);
+	});
+
+	$('#preview').click(function(e){
+		e.preventDefault();
+		var form = $('#ballotForm').serialize();
+		if(form == ''){
+			$('.message').html('You must vote atleast one candidate');
+			$('#alert').show();
+		}
+		else{
+			$.ajax({
+				type: 'POST',
+				url: 'preview.php',
+				data: form,
+				dataType: 'json',
+				success: function(response){
+					if(response.error){
+						var errmsg = '';
+						var messages = response.message;
+						for (i in messages) {
+							errmsg += messages[i]; 
+						}
+						$('.message').html(errmsg);
+						$('#alert').show();
+					}
+					else{
+						$('#preview_modal').modal('show');
+						$('#preview_body').html(response.list);
+					}
+				}
+			});
+		}
+		
+	});
+
+});
+</script>
 </body>
 </html>
