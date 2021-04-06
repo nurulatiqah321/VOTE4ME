61d6a2387595c5995d55ce7c8a9097c063caa301
Nurul Atiqah <48888801+nurulatiqah321@users.noreply.github.com>
2021-04-06 22:41:54 +0800

    Delete admin directory

--- admin/includes/conn.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$conn = new mysqli('localhost', 'root', 'xhqxHVzH', 'vote4me');
-
-	if ($conn->connect_error) {
-	    die("Connection failed: " . $conn->connect_error);
-	}
-	
-?>
--- admin/includes/footer.php
+++
@@ -1,7 +0,0 @@
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-      <b>All rights reserved</b>
-    </div>
-    <!-- <strong>Copyright &copy; 2018 <a href="https://www.sourcecodester.com/">SourceCodeSter</a></strong> -->
-    <strong>Copyright &copy; 2021</strong>
-</footer>
